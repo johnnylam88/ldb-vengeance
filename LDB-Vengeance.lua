afcf9fdc3342e62d09fe991edfe52a1b9636b218
jlam <jlam@edd2fcf2-b866-4837-9b38-a3eb0fb7f8be>
2013-05-23 04:52:04 +0000

    Reorganize and simplify this addon.
    * Sort code into sections to simplify navigation.
    
    * Rename Blizzard API functions to make it clear where in the code they
      are used.
    
    * Remove the saved variables and make broker always display a valid value.
    
    The various display options added verbosity without any clear benefits.
    LDB-Vengeance is meant to show the current value of Vengeance, so on toons
    where it's not available, just show 0 Vengeance.
    
    git-svn-id: svn+ssh://svn.wowace.com/wow/ldb-vengeance/mainline/trunk@56 edd2fcf2-b866-4837-9b38-a3eb0fb7f8be

--- LDB-Vengeance.lua
+++ LDB-Vengeance.lua
@@ -5,30 +5,31 @@
 ]]--
 
 local addonName, ns = ...
-local addon = CreateFrame("Frame", addonName)
 local L = LibStub:GetLibrary("AceLocale-3.0"):GetLocale(addonName)
 
-local addonversion ="@project-version@"
+local API_CreateFrame = CreateFrame
+local API_GetAddOnMetadata = GetAddOnMetadata
+local API_GetSpellBookItemInfo = GetSpellBookItemInfo
+local API_GetSpellInfo = GetSpellInfo
+local API_UnitAura = UnitAura
+local API_UnitClass = UnitClass
 
-local DBversion = "2"
+local select = select
+local tonumber = tonumber
 
-local defaultText = "|cffC79C6ELDB|r-Vengeance"
-local defaultIcon = "Interface\\Icons\\Ability_Paladin_ShieldofVengeance"
-
-local LDBVengeance = LibStub("LibDataBroker-1.1"):NewDataObject(
-	addonName, 
-	{ 
-		icon = defaultIcon, 
-		type = "data source",
-		text = defaultText
-	}
-)
+local addonVersion = API_GetAddOnMetadata(addonName, "Version")
+local addon = API_CreateFrame("Frame", addonName)
+local defaultLDB = {
+	icon = "Interface\\Icons\\Ability_Paladin_ShieldofVengeance",
+	type = "data source",
+	text = 0,
+	value = 0,
+}
+local LDBVengeance = LibStub("LibDataBroker-1.1"):NewDataObject(addonName, defaultLDB)
 
 local isTank = false
-local playerClass = select(2, UnitClass("player"))
-local vengeanceSpellName = GetSpellInfo(93098)	-- known Vengeance spell ID
-
-local GetSpellBookItemInfo, UnitAura = GetSpellBookItemInfo, UnitAura
+local playerClass = select(2, API_UnitClass("player"))
+local vengeanceSpellName = API_GetSpellInfo(93098)	-- known Vengeance spell ID
 
 -- Table of classes with a tanking specialization.
 local tankClass = {
@@ -39,74 +40,28 @@ local tankClass = {
 	WARRIOR = true,
 }
 
--- Set the LDB display values.
-function addon:SetLDBDisplay(text, icon)
-	LDBVengeance.text = text
-	LDBVengeance.icon = icon
-
-	local value = tonumber(text) or 0
-	if LDBVengeanceDB.provideValue then
-		LDBVengeance.value = value
-	else
-		LDBVengeance.value = false
+local function EventHandler(self, event, ...)
+	if self[event] then
+		self[event](self, event, ...)
 	end
 end
 
-function addon:UpdateTankStatus()
-	-- Player is considered to be in a tanking specialization if the passive spell
-	-- "Vengeance" is present in the player's spellbook.
-	local spellId = select(2, GetSpellBookItemInfo(vengeanceSpellName))
-	if spellId then
-		isTank = true
-		self:RegisterEvent('UNIT_AURA')
-	else
-		isTank = false		
-		self:UnregisterEvent('UNIT_AURA')
-	end
+-- Addon initialization.
+do
+	addon:RegisterEvent(IsAddOnLoaded('AddonLoader') and 'ADDON_LOADED' or 'PLAYER_LOGIN')
+	addon:SetScript('OnEvent', EventHandler)
 end
 
-function addon:GetVengeance()
-	local _, _, icon, _, _, _, _, _, _, _, _, _, _, _, value = UnitAura("player", vengeanceSpellName)
-
-	icon = icon or LDBVengeance.icon or defaultIcon
-	value = value or 0
-
-	return value, icon
+-- Frame events.
+function LDBVengeance:OnTooltipShow()
+	self:AddLine("|cff00ff00"..addonName.." "..addonVersion.."|r")
+	self:AddLine("|cffffffff"..L['Displays the current value of your vengeance buff'].."|r")
 end
 
-function addon:UNIT_AURA(event, unit)
-	if unit == "player" and isTank then
-		self:SetLDBDisplay(self:GetVengeance())
-	end
-end
-
+-- Game events.
 function addon:ACTIVE_TALENT_GROUP_CHANGED()
 	self:UpdateTankStatus()
 	self:SetLDBDisplay(self:GetVengeance())
-end
-
-function addon:UpgradeSavedVariables()
-	-- Upgrade saved variables if necessary.
-	if (not LDBVengeanceDB or not LDBVengeanceDB.dbVersion or LDBVengeanceDB.dbVersion < DBversion) then
-		LDBVengeanceDB = LDBVengeanceDB or {}
-		LDBVengeanceDB.dbVersion = DBversion
-		LDBVengeanceDB.defaultText = LDBVengeanceDB.defaultText or defaultText
-		LDBVengeanceDB.provideValue = LDBVengeanceDB.provideValue or 1
-	end
-end
-
-function addon:PLAYER_LOGIN()
-	-- At this point, the saved variables are loaded.
-	self:UpgradeSavedVariables()
-
-	-- Set the default LDB display values when entering the world.
-	self:SetLDBDisplay(LDBVengeanceDB.defaultText, defaultIcon)
-
-	if tankClass[playerClass] then
-		self:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
-		self:UpdateTankStatus()
-		self:SetLDBDisplay(self:GetVengeance())
-	end
 end
 
 function addon:ADDON_LOADED(event, name)
@@ -117,78 +72,53 @@ function addon:ADDON_LOADED(event, name)
 	end
 end
 
-function LDBVengeance:OnTooltipShow()
-	self:AddLine(defaultText.." |cff00ff00"..addonversion.."|r")
-	self:AddLine("|cffffffff"..L['Displays the current value of your vengeance buff'].."|r")
-	if not tankClass[playerClass] then
-		self:AddLine("|cffff0000"..L['Note: This addon does not make any sense for classes that don\'t have a Vengeance buff'].."|r")
+function addon:PLAYER_LOGIN()
+	-- Set the default LDB display values when entering the world.
+	self:SetLDBDisplay(defaultLDB.value, defaultLDB.icon)
+
+	if tankClass[playerClass] then
+		self:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
+		self:RegisterEvent('UNIT_LEVEL')
+		self:UpdateTankStatus()
+		self:SetLDBDisplay(self:GetVengeance())
 	end
 end
 
-function LDBVengeance:OnClick(msg)
-	if msg == "RightButton" then
-		InterfaceOptionsFrame_OpenToCategory(addon.optionsFrame)
+function addon:UNIT_AURA(event, unit)
+	if unit == "player" and isTank then
+		self:SetLDBDisplay(self:GetVengeance())
 	end
 end
 
-local options = {
-	type ="group",
-	name = defaultText,
-	handler = LDBVengeance,
-	childGroups = "tree",
-	args = {
-		header1 = {
-			type = "description",
-			name = L["Some useful description"],
-			order = 0,
-			width = "full",
-			cmdHidden = true
-		},
-		spacer1 = {
-			type = "header",
-			name = "",
-			order = 1,
-			width = "full",
-			cmdHidden = true
-		},
-		provideValue = {
-			type = "toggle",
-			name = L["Provide a 'value' for the LDB display"],
-			width = "full",
-			order = 2,
-			set = function (info, value)
-				LDBVengeanceDB.provideValue = value
-			end,
-			get = function() return LDBVengeanceDB.provideValue end
-		},
-		defaultText = {
-			type = "input",
-			name = L["Default data source text"],
-			width = "double",
-			order = 3,
-			set = function (info,value)
-				LDBVengeanceDB.defaultText = value
-			end,
-			get = function() return LDBVengeanceDB.defaultText end
-		},
-		resetToDefaultText = {
-			type = "execute",
-			name = L["Reset"],
-			desc = L["Reset default text (|cffC79C6ELDB|r-Vengeance)"],
-			order = 4,
-			func = function() LDBVengeanceDB.defaultText = defaultText end,
-		},
-	},
-}
-
-LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, options)
-addon.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions(addonName)
-
-local function EventHandler(self, event, ...)
-	if self[event] then
-		self[event](self, event, ...)
+function addon:UNIT_LEVEL(event, unit)
+	if unit == "player" then
+		self:ACTIVE_TALENT_GROUP_CHANGED()
 	end
 end
 
-addon:RegisterEvent(IsAddOnLoaded('AddonLoader') and 'ADDON_LOADED' or 'PLAYER_LOGIN')
-addon:SetScript('OnEvent', EventHandler)
+-- Public methods.
+function addon:GetVengeance()
+	local _, _, icon, _, _, _, _, _, _, _, _, _, _, _, value = API_UnitAura("player", vengeanceSpellName)
+	icon = icon or defaultLDB.icon
+	value = value or 0
+	return value, icon
+end
+
+function addon:SetLDBDisplay(value, icon)
+	LDBVengeance.icon = icon
+	LDBVengeance.value = value or 0
+	LDBVengeance.text = LDBVengeance.value
+end
+
+function addon:UpdateTankStatus()
+	-- Player is considered to be in a tanking specialization if the passive spell
+	-- "Vengeance" is present in the player's spellbook.
+	local spellId = select(2, API_GetSpellBookItemInfo(vengeanceSpellName))
+	if spellId and tankClass[playerClass] then
+		isTank = true
+		self:RegisterEvent('UNIT_AURA')
+	else
+		isTank = false
+		self:UnregisterEvent('UNIT_AURA')
+	end
+end
